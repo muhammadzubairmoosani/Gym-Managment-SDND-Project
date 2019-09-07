35681af0ad70ac17c07acd00d076686294762cf2
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2019-09-07 16:18:53 -0700

    first commit

--- gymProject/viewMembersDetail.cs
+++ gymProject/viewMembersDetail.cs
@@ -29,6 +29,7 @@ namespace gymProject
             DataColumn dc11 = new DataColumn("Year");
             DataColumn dc12 = new DataColumn("Total Amount");
             DataColumn dc13 = new DataColumn("MemberShip Duration");
+            DataColumn dc14 = new DataColumn(" ");
             dt.Columns.Add(dc1);
             dt.Columns.Add(dc2);
             dt.Columns.Add(dc3);
@@ -42,6 +43,7 @@ namespace gymProject
             dt.Columns.Add(dc11);
             dt.Columns.Add(dc12);
             dt.Columns.Add(dc13);
+            dt.Columns.Add(dc14);
             dataGrid.DataSource = dt;
 
             string[] values;
@@ -65,26 +67,17 @@ namespace gymProject
                 bool flag = true;
                 for (int i = 0; i < dataGrid.RowCount - 1; i++)
                 {
-                    if (dataGrid.Rows[i].Cells[0].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[1].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[2].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[3].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[4].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[5].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[6].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[7].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[8].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[9].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[10].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[11].Value.ToString().Equals(searchValue.Text.ToLower()) ||
-                        dataGrid.Rows[i].Cells[12].Value.ToString().Equals(searchValue.Text.ToLower()))
+                    for (int j = 0; j <= 11; j++)
                     {
-                        for (int j = 0; j < 13; j++)
+                        if (dataGrid.Rows[i].Cells[j].Value.ToString().Equals(searchValue.Text.ToLower()))
                         {
-                            dataGrid.Rows[i].Cells[j].Selected = true;
+                            for (int k = 0; k < 13; k++)
+                            {
+                                dataGrid.Rows[i].Cells[k].Selected = true;
+                            }
+                            flag = false;
+                            userId = dataGrid.Rows[i].Cells[0].Value.ToString();
                         }
-                        flag = false;
-                        userId = dataGrid.Rows[i].Cells[0].Value.ToString();
                     }
                 }
                 if (flag)
@@ -101,12 +94,28 @@ namespace gymProject
 
         internal void deleteRecord(DataGridView dataGrid)
         {
+            bool flag = false;
             for (int i = 0; i < dataGrid.RowCount - 1; i++)
             {
                 if (dataGrid.Rows[i].Cells[0].Value.ToString().Equals(userId))
                 {
                     dataGrid.Rows.RemoveAt(dataGrid.Rows[i].Index);
                     MessageBox.Show("Delete Record Successfully!");
+                    flag = !flag;
+                }
+            }
+            if (flag)
+            {
+                using (StreamWriter write = new StreamWriter("membersDetail.txt"))
+                {
+                    for (int i = 0; i < dataGrid.RowCount - 1; i++)
+                    {
+                        for (int j = 0; j < dataGrid.ColumnCount; j++)
+                        {
+                            write.Write(dataGrid.Rows[i].Cells[j].Value.ToString() + ", ");
+                        }
+                        write.WriteLine();
+                    }
                 }
             }
         }
